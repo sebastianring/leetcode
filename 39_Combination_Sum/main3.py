da7a8a00ca8a8b325dae4d5f2cf94080014bb108
sebastian <sebastianring@gmail.com>
2023-01-17 10:13:26 +0100

    added solution for #39

--- 39_Combination_Sum/main3.py
+++ 39_Combination_Sum/main3.py
@@ -5,25 +5,27 @@ class Solution:
         r_list = []
         cand_length = len(candidates)
 
-        def rec_add(ctr=0, cur_sum=0, cur_list=[]):
-            cur_sum+=candidates[ctr]
-            cur_list.append(candidates[ctr])
+        def rec_add(ctr: int, cur_sum: int, cur_list=None):
+            if cur_list==None:
+                cur_list = []
+                cur_list.append(candidates[i])
 
-            print(target, cur_sum, cur_list)
-            for next_ctr in range(ctr+1, cand_length):
-                if cur_sum + candidates[next_ctr] <= target:
-                    rec_add(next_ctr, cur_sum, cur_list)
-
-            if cur_sum + candidates[ctr] <= target:
-                rec_add(ctr, cur_sum, cur_list)
-
-            if cur_sum == target:
+            if cur_sum==target:
                 r_list.append(cur_list)
-            
-            return
+                return
+            elif cur_sum > target:
+                return
+            else:
+                for nextctr in range(ctr, cand_length):
+                    next_sum = cur_sum + candidates[nextctr]
+                    if next_sum <= target:
+                        new_list = list(cur_list)
+                        new_list.append(candidates[nextctr])
+                        rec_add(nextctr, next_sum, new_list)
+
 
         for i in range(cand_length):
-            rec_add(ctr=i)
+            rec_add(i, candidates[i])
 
         return r_list
 
@@ -33,7 +35,7 @@ class Solution:
 
 
 p = Solution()
-# print(p.combinationSum(candidates = [2, 3, 6, 7], target = 7))
-# print(p.combinationSum(candidates = [2, 3, 5], target = 8))
-# print(p.combinationSum(candidates = [8, 7, 4, 3], target = 11)) #[8, 3], [7, 4], [4,4,3]
+print(p.combinationSum(candidates = [2, 3, 6, 7], target = 7))
+print(p.combinationSum(candidates = [2, 3, 5], target = 8))
+print(p.combinationSum(candidates = [8, 7, 4, 3], target = 11)) #[8, 3], [7, 4], [4,4,3]
 print(p.combinationSum(candidates = [7, 3, 2], target = 18)) # [[7,7,2,2],[7,3,3,3,2],[7,3,2,2,2,2],[3,3,3,3,3,3],[3,3,3,3,2,2,2],[3,3,2,2,2,2,2,2],[2,2,2,2,2,2,2,2,2]]
