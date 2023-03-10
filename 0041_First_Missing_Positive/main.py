217ad707bc0518a93f693ad9e63951397497c79a
sebastian <sebastianring@gmail.com>
2023-03-10 11:43:44 +0100

    updated folder names

--- 0041_First_Missing_Positive/main.py
+++ 0041_First_Missing_Positive/main.py
@@ -1,6 +1,8 @@
-# Given an unsorted integer array nums, return the smallest missing positive integer.
+# Given an unsorted integer array nums,
+# return the smallest missing positive integer.
 
-# You must implement an algorithm that runs in O(n) time and uses constant extra space.
+# You must implement an algorithm that
+# runs in O(n) time and uses constant extra space.
 
 # Example 1:
 
@@ -24,7 +26,7 @@
 class Solution:
     def firstMissingPositive(self, nums: list[int]) -> int:
         # temp_list = []
-        temp_num = 1 
+        temp_num = 1
 
         for n in nums:
             if n > 0:
@@ -33,15 +35,10 @@ class Solution:
                 # temp_list.append(n)
 
         return temp_num
-   
-
-
-
-
 
 
 p = Solution()
-print(p.firstMissingPositive(nums = [1,2,0]))
-print(p.firstMissingPositive(nums = [3,4,-1,1]))
-print(p.firstMissingPositive(nums = [7,8,9,11,12]))
-print(p.firstMissingPositive(nums = [2, 1]))
+print(p.firstMissingPositive(nums=[1, 2, 0]))
+print(p.firstMissingPositive(nums=[3, 4, -1, 1]))
+print(p.firstMissingPositive(nums=[7, 8, 9, 11, 12]))
+print(p.firstMissingPositive(nums=[2, 1]))
